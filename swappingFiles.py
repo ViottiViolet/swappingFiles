f8487a338139115e918b99f9cd77e8c518619c5d
ViottiViolet <80078910+ViottiViolet@users.noreply.github.com>
2022-04-05 18:30:02 -0500

    Add files via upload

---
+++ swappingFiles.py
@@ -0,0 +1,11 @@
+a = open("file1.txt", "r")
+data_a = a.read()
+
+b = open("file2.txt", "r")
+data_b = b.read()
+
+a = open("file1.txt", "w")
+a.write(data_b)
+
+b = open("file2.txt", "w")
+b.write(data_a)
